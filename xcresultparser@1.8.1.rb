58f4a32a669cb4365de5d30bbe116c1e159277ff
Alex da Franca <adf@starfinanz.de>
2024-10-16 15:36:09 +0200

    New release 1.8.1 of xcresultparser

--- xcresultparser@1.8.1.rb
+++ xcresultparser@1.8.1.rb
@@ -1,4 +1,4 @@
-class Xcresultparser < Formula
+class XcresultparserAT181 < Formula
   desc "Parse .xcresult files and print summary in different formats"
   homepage "https://github.com/a7ex/xcresultparser"
   version "1.8.1"
